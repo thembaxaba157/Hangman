9867abf59df691c41957b6004807c448bf48739c
thembaxaba157 <thembaxaba157@gmail.com>
2025-03-07 20:21:54 +0200

    chore(words.json): added a new json file that contains 50+ words along with categories and difficulty

---
+++ README.md
@@ -0,0 +1,5 @@
+things used/will be used so far
+- sqlite
+- junit and mockito for testing
+- sementic versioning and workflows
+- json for words
---
+++ src/main/java/com/example/Hangman.java
@@ -0,0 +1,15 @@
+package com.example;
+
+public class Hangman {
+    
+
+    public Hangman(){
+
+    }
+
+    public void run(){
+        
+    }
+
+
+}
